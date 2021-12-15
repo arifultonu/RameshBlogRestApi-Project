3fd704c119a5bc4e12430d276fd096c76281a9c3
Ahnaf Tahseen Prince <princekenway@gmail.com>
2021-12-15 16:34:06 +0600

    Implemented Post request
    now student record can be inserted in the database

--- StudentInfoCrud/src/main/java/com/example/demo/student/StudentService.java
+++ StudentInfoCrud/src/main/java/com/example/demo/student/StudentService.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Service;
 import java.time.LocalDate;
 import java.time.Month;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class StudentService {
@@ -22,4 +23,15 @@ public class StudentService {
 
         return studentRepository.findAll();
     }
+
+    public void addNewStudent(Student student) {
+
+        Optional<Student> studentByEmail = studentRepository.findStudentByEmail(student.getEmail());
+        if(studentByEmail.isPresent()){
+            throw new IllegalStateException("this email is taken already!");
+        }
+        studentRepository.save(student);
+
+    }
+
 }
