31da099e256fcb5c9907cadf1c1dc380e04da37b
Ahnaf Tahseen Prince <princekenway@gmail.com>
2021-12-30 13:43:47 +0600

    Get Post By Id REST API done

--- springboot-blog-rest-api/src/main/java/com/springboot/blog/service/impl/PostServiceImpl.java
+++ springboot-blog-rest-api/src/main/java/com/springboot/blog/service/impl/PostServiceImpl.java
@@ -1,6 +1,7 @@
 package com.springboot.blog.service.impl;
 
 import com.springboot.blog.entity.Post;
+import com.springboot.blog.exception.ResourceNotFoundException;
 import com.springboot.blog.payload.PostDto;
 import com.springboot.blog.repository.PostRepository;
 import com.springboot.blog.service.PostService;
@@ -44,7 +45,9 @@ public class PostServiceImpl implements PostService {
 
     @Override
     public PostDto getPostById(Long id) {
-        return null;
+        Post post = postRepository.findById(id).orElseThrow(() ->
+                new ResourceNotFoundException("Post", "id", id));
+        return mapToDto(post);
     }
 
     //convert DTO to Entity
