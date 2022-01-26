37ad9ceae3af3f8c4900bb91daf87bfcc5467ff6
Ahnaf Tahseen Prince <princekenway@gmail.com>
2022-01-26 18:02:53 +0600

    swagger added with basic security

--- springboot-blog-rest-api/src/main/java/com/springboot/blog/SpringbootBlogRestApiApplication.java
+++ springboot-blog-rest-api/src/main/java/com/springboot/blog/SpringbootBlogRestApiApplication.java
@@ -1,9 +1,12 @@
 package com.springboot.blog;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "Client Service APIs", version = "1.0", description = "E-Com Microservice"))
 public class SpringbootBlogRestApiApplication {
 
 	public static void main(String[] args) {
